f79a584e546f2d10a8a0f402e94703741056ac85
john mbugua <johnymbugua1997@gmail.com>
2024-02-17 23:25:39 +0300

    react project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ParentRouter from "./components/parentrouter";
+
+const App = () => {
+  return(
+    <>
+    {/* for importing parentRouter component below */}
+      <ParentRouter/>
+    </>
+  )
+
+}
+
+export default App;
